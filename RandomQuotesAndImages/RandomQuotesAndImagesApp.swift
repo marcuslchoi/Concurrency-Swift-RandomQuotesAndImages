ddc0d57ca5dba557eb3eef1179a132d3108108ff
marcuslchoi <marcuslchoi@gmail.com>
2022-01-11 12:23:57 -0800

    Initial Commit

---
+++ RandomQuotesAndImages/RandomQuotesAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuotesAndImagesApp.swift
+//  RandomQuotesAndImages
+//
+//  Created by Marcus Choi on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuotesAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
